9d597ea9e8758c357806e441898f36add5546b4f
Manmeet Kalra <manmeetskalra@gmail.com>
2024-01-11 18:26:22 +0530

    Time: 0 ms (100.00%), Space: 44.6 MB (33.25%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
@@ -14,35 +14,17 @@
  * }
  */
 class Solution {
-    int max = Integer.MIN_VALUE;
     public int maxPathSum(TreeNode root) {
-        if(root.left==null && root.right==null) return root.val;
-        helper(root);
-        // if(left<0 && right<0){
-        //     return root.val;
-        // }
-        // if(left < 0){
-        //     return root.val + right;
-        // }
-        // if(right <0) {
-        //     return root.val + left;
-        // }
-        // return root.val + Math.max(left,right);
-        return max;
+        int[] res = new int[1];
+        res[0] = Integer.MIN_VALUE;
+        helper(root, res);
+        return res[0];
     }
-    public int helper(TreeNode root){
-        if(root.left==null && root.right==null) return root.val;
-        int left = root.left!=null?helper(root.left): Integer.MIN_VALUE;
-        int right = root.right!=null?helper(root.right): Integer.MIN_VALUE;
-        int sum = 0;
-        if(left>0){
-            sum+=left;
-        }
-        if(right>0){
-            sum+=right;
-        }
-        int currMax = Math.max(sum+root.val,Math.max(left,right));
-        max = Math.max(max,currMax);
-        return Math.max(root.val,root.val+Math.max(left,right));
+    public int helper(TreeNode root, int[] res){
+        if(root==null) return 0;
+        int left = Math.max(0,helper(root.left, res));
+        int right = Math.max(0,helper(root.right, res));
+        res[0] = Math.max(res[0],left+right+root.val);
+        return root.val+Math.max(left,right);
     }
 }
