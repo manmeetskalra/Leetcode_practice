cd50e09d9b98b45ad5ea4fad3fa2a4a4ecafed32
Manmeet Kalra <manmeetskalra@gmail.com>
2024-01-20 18:24:03 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (48.80%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -7,19 +7,11 @@ class Solution {
         for(int i=0;i<n;i++){
             dp[0][i] = 1;
         }
-        startFilling(dp,m-1,n-1);
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                dp[i][j] = dp[i-1][j]+dp[i][j-1];
+            }
+        }
         return dp[m-1][n-1];
-    }
-    public void startFilling(int[][] dp, int i, int j){
-        if(i<1 || i>dp.length-1 || j<1 || j>dp[i].length-1 || dp[i][j]!=0) return;
-        
-        if(dp[i-1][j]==0){
-            startFilling(dp,i-1,j);
-        }
-        if(dp[i][j-1]==0){
-            startFilling(dp,i,j-1);
-        }
-        dp[i][j] = dp[i-1][j] + dp[i][j-1];
-        return;
     }
 }
