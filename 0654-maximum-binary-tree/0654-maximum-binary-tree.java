ddcb3993bbb8d5d00c3aa3fd8e7c7a1cee4812bd
Manmeet Kalra <manmeetskalra@gmail.com>
2024-02-08 01:10:07 +0530

    Time: 45 ms (6.29%), Space: 44.6 MB (68.94%) - LeetHub

---
+++ 0654-maximum-binary-tree/0654-maximum-binary-tree.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode constructMaximumBinaryTree(int[] nums) {
+        return recursive(nums,0,nums.length-1);
+    }
+    public TreeNode recursive(int[] nums, int start, int end){
+        if(start>end){
+            return null;
+        }
+        if(start==end){
+            return new TreeNode(nums[start]);
+        }
+        int index = -1;
+        Stack<Integer> stack = new Stack<>();
+        for(int i=end;i>=start;i--){
+            if(!stack.isEmpty() && nums[i]>nums[stack.peek()]){
+                while(!stack.isEmpty() && nums[i]>nums[stack.peek()]){
+                    stack.pop();
+                }
+            }
+            if(stack.isEmpty()){
+                index = i;
+            }
+            stack.push(i);
+        }
+        TreeNode root = new TreeNode(nums[index]);
+        root.left = recursive(nums,start,index-1);
+        root.right = recursive(nums,index+1,end);
+        return root;
+    }
+}
