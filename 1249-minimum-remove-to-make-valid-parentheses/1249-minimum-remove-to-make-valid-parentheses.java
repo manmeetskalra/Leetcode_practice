2f5a26ea5b5c67c54be7e1174eeebf609beea3dc
Manmeet Kalra <manmeetskalra@gmail.com>
2024-02-14 15:59:49 +0530

    Time: 11 ms (91.69%), Space: 45.2 MB (70.58%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
@@ -1,41 +1,23 @@
 class Solution {
     public String minRemoveToMakeValid(String s) {
-        StringBuffer sb = new StringBuffer();
-        Stack<Character> stack = new Stack<>();
-        int open = 0, close = 0;
+        int open = 0;
+        StringBuilder sb = new StringBuilder();
         for(int i=0;i<s.length();i++){
             char c = s.charAt(i);
-            if(c!='(' && c!=')'){
-                stack.push(c);
-            } else{
-                if(c==')'){
-                    if(open == close){
-                        continue;
-                    }else{
-                        stack.push(c);
-                        close++;
-                    }
-                } else{
-                    stack.push(c);
-                    open++;
-                }
-            }
-        }
-        open = close;
-        int currClose = 0;
-        while(!stack.isEmpty()){
-            char c = stack.pop();
             if(c=='('){
-                if(open==0 || currClose==0){
+                open++;
+            } else if(c==')'){
+                if(open==0){
                     continue;
                 }else{
                     open--;
-                    currClose--;
                 }
-            } else if(c==')'){
-                currClose++;
             }
-            sb.insert(0,c);
+            sb.append(c);
+        }
+        while(open>0){
+            sb.deleteCharAt(sb.lastIndexOf("("));
+            open--;
         }
         return sb.toString();
     }
