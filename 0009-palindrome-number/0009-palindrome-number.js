6c3429d8289adb842d77b993a242c6da40429553
Manmeet Kalra <manmeetskalra@gmail.com>
2024-04-12 16:37:31 +0530

    Time: 141 ms (39.65%), Space: 57 MB (73.45%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    var str = x.toString();
+    let i=0;
+    let j=str.length-1;
+    
+    while(i<j){
+        if(str[i]!=str[j]){
+            return false;
+        }
+        i++;
+        j--;
+    }
+    
+    return true;
+    
+};
